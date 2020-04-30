a71b8097dda38d34176a6ca002872a957fd83b39
hsahraou@etud.insa-toulouse.fr <hsahraou@etud.insa-toulouse.fr>
2020-04-30 09:26:30 +0200

    MAJ Xilinx

--- VHDL/bancregistre.vhd
+++ VHDL/bancregistre.vhd
@@ -1,6 +1,18 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+
 
 entity bancregistre is
     Port ( A : in  STD_LOGIC_VECTOR (3 downto 0);
