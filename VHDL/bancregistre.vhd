62aac67be3b18da775691f95afd0fff9ce427103
hsahraou@etud.insa-toulouse.fr <hsahraou@etud.insa-toulouse.fr>
2020-05-04 11:57:49 +0200

    Correction du banc de registre

--- VHDL/bancregistre.vhd
+++ VHDL/bancregistre.vhd
@@ -1,9 +1,9 @@
 --ERREURn probleme avec les Librairies.......
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+--use IEEE.STD_LOGIC_ARITH.ALL;
 --use IEEE.STD_LOGIC_UNSIGNED.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -31,8 +31,8 @@ end bancregistre;
 
 architecture Behavioral of bancregistre is
 
-type registreTableau is array (integer range 15 downto 0) of STD_LOGIC_VECTOR(7 downto 0);
-signal registr : registreTableau := (others => (others => '0')); --Attention on peut pas utiliser registre sinon error
+type registreTableau is array (integer range <>) of STD_LOGIC_VECTOR(7 downto 0);
+signal registr : registreTableau; --Attention on peut pas utiliser registre sinon error
 
 begin
 --Partie Asynchrone
@@ -40,21 +40,15 @@ begin
 
 --https://www.xilinx.com/support/answers/45213.html
 --Ajouter ici un when avec  data et w 
-	if (W = '1') then
-		if addW = A  then
-			QA <= Data;
-		end if;
-		if addW = B then
-			QB <= Data;
-		end if;
-	else
-		QA <= registr(to_integer(unsigned(A)));
-		QB <= registr(to_integer(unsigned(B)));
-	end if;
+	QA <= Data when addW = A AND W = '1' else
+			registr(to_integer(unsigned(A))) when W = '0';
+	QB <= Data when addW = B AND W = '1' else
+			registr(to_integer(unsigned(B))) when W = '0';
+
 
 --On considère que le reset et l'écriture se feront synchrone avec l'horloge.
-	process
-	begin
+		process
+		begin
 		wait until CLK'event and CLK='1';
 -- Le signal reset RST est actif à 0 : le contenu du banc de registres est alors initialisé à 0x00.
 			if RST = '0' then
@@ -62,7 +56,7 @@ begin
 			else
 -- Cette entrée est active à 1, pour une écriture. Lorsque l'écriture est activée, les données présentent sur l'entrée DATA sont copiées dans le registre d'adresse @W
 				if W = '1' then
-					registr(to_integer(addW)) <= Data;
+					registr(to_integer(unsigned(addW))) <= Data;
 				end if;
 			end if;
 		
