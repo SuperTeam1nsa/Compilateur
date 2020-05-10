2933438b3834e714a911445a67f83f434de358eb
hsahraou@etud.insa-toulouse.fr <hsahraou@etud.insa-toulouse.fr>
2020-05-11 00:16:32 +0200

    first push de la soiree

---
+++ VHDL/pipeline1.vhd
@@ -0,0 +1,45 @@
+--En cours
+
+library IEEE;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pipeline1 is
+Port (     CLK : in  STD_LOGIC;
+		   OP : in  STD_LOGIC_VECTOR (7 downto 0);
+           A : in  STD_LOGIC_VECTOR (7 downto 0);
+           B : in  STD_LOGIC_VECTOR (7 downto 0);
+           C : in  STD_LOGIC_VECTOR (7 downto 0);
+           OOP : out  STD_LOGIC_VECTOR (7 downto 0);
+           OA : out  STD_LOGIC_VECTOR (7 downto 0);
+           OB : out  STD_LOGIC_VECTOR (7 downto 0);
+           OC : out  STD_LOGIC_VECTOR (7 downto 0));
+
+end pipeline1;
+
+architecture Behavioral of pipeline1 is
+
+begin
+ process 
+	begin
+	
+		wait until CLK'event and CLK = '1';
+		OOP <= OP;
+		OA <= A;
+		OB <= B;
+		OC <= C;		
+
+	end process;
+
+
+end Behavioral;
