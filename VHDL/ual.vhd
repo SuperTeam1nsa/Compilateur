bbce181e95cb9404a764ec270c987e1528e05501
hsn31 <39495417+hsn31@users.noreply.github.com>
2020-05-19 14:13:09 +0200

    Update ual.vhd

--- VHDL/ual.vhd
+++ VHDL/ual.vhd
@@ -1,13 +1,13 @@
 --Compilation OK
 
 library IEEE;
-use IEEE.STD_LOGIC_ARITH.ALL;
+--use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
